6c4debc21664841fcb2f16fdf17556521209a1ca
Fatih Sukran <fatih.sukran1@gmail.com>
2023-07-26 15:51:28 +0300

    Initial Commit

---
+++ financel assets/financel_assetsApp.swift
@@ -0,0 +1,17 @@
+//
+//  financel_assetsApp.swift
+//  financel assets
+//
+//  Created by Fatih Şükran on 26.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct financel_assetsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
