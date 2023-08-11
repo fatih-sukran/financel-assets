ea660fc768e1a103538dae637d22ea3dd7aa0707
fatih-sukran <fth.21.81@gmail.com>
2023-08-11 21:02:09 +0300

    add price form

---
+++ financel assets/PriceFormView.swift
@@ -0,0 +1,20 @@
+//
+//  PriceFormView.swift
+//  financel assets
+//
+//  Created by Fatih Şükran on 31.07.2023.
+//
+
+import SwiftUI
+
+struct PriceFormView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct PriceFormView_Previews: PreviewProvider {
+    static var previews: some View {
+        PriceFormView()
+    }
+}
